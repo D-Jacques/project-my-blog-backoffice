c876233b1b67f61dbfa420a30668a7272d971216
D-Jacques <jacques.ducroux@gmail.com>
2025-12-20 23:10:38 +0100

    Creating a new ui component for our article form, implementing it on the page.tsx file in article/create/

--- blog-backoffice/src/app/articles/create/page.tsx
+++ blog-backoffice/src/app/articles/create/page.tsx
@@ -5,6 +5,7 @@
 "use client";
 
 import { createArticle, State } from "@/app/lib/articleActions";
+import Form from "@/app/ui/createArticleForm";
 import { useActionState } from "react";
 
 export default function ArticlesCreatePage () {
@@ -12,41 +13,16 @@ export default function ArticlesCreatePage () {
      * The initial state of our form, we arrive here with no errors and no message
      * as the user has not interacted with the form yet
      */
-    const initialState : State = { message: null, errors: {} };
-    const [state, formAction] = useActionState(createArticle, initialState)
+    // const initialState : State = { message: null, errors: {} };
+    // const [state, formAction] = useActionState(createArticle, initialState)
 
     return (
-        <div>
-            {/* 
-                Ici on aura un <main></main> et dedans on incluera notre fragment UI form
-                Récupérer la partie formulaire et la placer dans un fichier ui
-                "create-article-form.tsx"
-                Déplacer la logique state formaction dans le composant
-            
-            */}
-            <div className="">
+            // La il faudra que toutes mes pages aient un side menu a 1/4 et un contenu a 3/4
+            <main>
                 <h1>Create an Article</h1>
                 <div className="w-full ">
-                    {/* Place this code into a component later => create-form.tsx */}
-                    <form className="bg-white shadow-md mx-5 px-5 py-4" action={formAction}>
-                        <div className="relative mb-4">
-                            <label htmlFor="title" className="block text-grey-700">Titre de l'article</label>
-                            <input type="text" name="title" id="title" className="w-full rounded border border-gray-400"/>
-                        </div>
-
-                        <div className="relative mb-4">
-                            <label htmlFor="content" className="block text-grey-700">Contenu de l'article</label>
-                            <textarea name="content" id="content" rows={10} className="w-full rounded border border-gray-400"></textarea>
-                        </div>
-
-                        {/* Later this value will be associated with the current user */}
-                        <input type="hidden" name="author" value={"Djack"} />
-
-{/* Problème de validation de formulaire, les données ne sont pas bien transmises ? */}
-                        <button type="submit">submit</button>
-                    </form>
+                    <Form />
                 </div>
-            </div>
-        </div>
+            </main>
     )
 }
---
+++ blog-backoffice/src/app/ui/createArticleForm.tsx
@@ -0,0 +1,28 @@
+"use client";
+
+import { useActionState } from "react";
+import { createArticle, State } from "../lib/articleActions";
+
+export default function Form () {
+    const initialState : State = { message: null, errors: {} };
+    const [state, formAction] = useActionState(createArticle, initialState)
+
+    return (
+        <form className="bg-white shadow-md mx-5 px-5 py-4" action={formAction}>
+            <div className="relative mb-4">
+                <label htmlFor="title" className="block text-grey-700">Titre de l'article</label>
+                <input type="text" name="title" id="title" className="w-full rounded border border-gray-400"/>
+            </div>
+
+            <div className="relative mb-4">
+                <label htmlFor="content" className="block text-grey-700">Contenu de l'article</label>
+                <textarea name="content" id="content" rows={10} className="w-full rounded border border-gray-400"></textarea>
+            </div>
+
+            <input type="hidden" name="author" value={"Djack"} />
+
+            <button type="submit">submit</button>
+        </form>
+    )
+
+}
