51530d9a8a49fa84b7904fe151194b1d597f1797
D-Jacques <jacques.ducroux@gmail.com>
2026-02-07 16:17:26 +0100

    Creating the index pages for articles, the articles are fetched from the API bakcoffice-api

---
+++ blog-backoffice/src/app/Types/Article.tsx
@@ -0,0 +1,7 @@
+export type Article = {
+    _id: string,
+    title: string,
+    image: string,
+    textContent: string,
+    author: string,
+}
--- blog-backoffice/src/app/articles/page.tsx
+++ blog-backoffice/src/app/articles/page.tsx
@@ -1,8 +1,61 @@
-export default function ArticlesList () {
+import Link from "next/link";
+import { Article } from "../Types/Article";
+
+
+async function getArticles() {
+    const baseApiUrl = process.env.BASE_API_URL as string;
+    const response = await fetch(`${baseApiUrl}/article/`);
+
+    const data = await response.json();
+    return data;    
+}
+
+export default async function ArticlesList () {
+
+    const articles = await getArticles();
+
     return (
-        <div>
+        <div className="">
             <h1>Articles List</h1>
             
+            <div id="container">
+
+                <h2>Articles existants</h2>
+
+                <div id="articles-list-container">
+                    <table className="mx-auto">
+                        <thead className="border-b">
+                            <tr>
+                                <th>Title</th>
+                                <th>Author</th>
+                                <th>Content</th>
+                                <th></th>
+                            </tr>
+                        </thead>
+        
+                        <tbody>
+                            {/* Liste des articles fetch depuis l'api */}
+                            {articles.map((article: Article, key: number) => {
+                                return (
+                                    <tr key={key} className="border-b">
+                                        <td>{article.title}</td>
+                                        <td>{article.author}</td>
+                                        <td>{article.textContent}</td>
+                                        <td>
+                                            <Link
+                                                href={`/articles/${article._id}/edit`}
+                                            >
+                                                editer
+                                            </Link>
+                                        </td>
+                                    </tr>
+                                )
+                            })}
+                        </tbody>
+                    </table>
+                </div> 
+            </div>
+
         </div>
     )
 }
